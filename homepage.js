613e26bb5bb0a1997464b09be81e88dd77de7550
WalkerX-C <forkanbe@gmail.com>
2026-04-07 08:55:54 +0800

    Add files via upload
    upload homepage files

---
+++ homepage.js
@@ -0,0 +1,13 @@
+function onfocusChange()
+{
+    document.getElementById("ob").style.backgroundColor = "red";
+    document.getElementById("ob").style.color = "yellow";
+}
+
+function onblurChange()
+{
+    document.getElementById("ob").style.backgroundColor = "white";
+    document.getElementById("ob").style.color = "black";
+}
+
+function 
